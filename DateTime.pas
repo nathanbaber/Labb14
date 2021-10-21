b479cf6f426314ac8f7035c08c55d42c5f432ff7
Glukhikh Anastasia <glukhikh@sfedu.ru>
2021-10-21 09:01:16 +0300

    added new function

--- DateTime.pas
+++ DateTime.pas
@@ -3,7 +3,11 @@
   begin
     if ((year mod 4 = 0) and (year mod 100 <> 0) or (year mod 100 = 0) and (year mod 400 = 0)) then IsLeapYear:= True;
   end;
- 
+  
+   function SecondsInMinute(min: integer) : integer;
+  begin
+    result:= min * 60;
+  end;
   
   begin
    
