0a0f5851bf4b80dd468502e938b99008adf5c122
Glukhikh Anastasia <glukhikh@sfedu.ru>
2021-10-21 08:59:50 +0300

    added new function

---
+++ DateTime.pas
@@ -0,0 +1,10 @@
+﻿
+  function IsLeapYear(year: integer): boolean;
+  begin
+    if ((year mod 4 = 0) and (year mod 100 <> 0) or (year mod 100 = 0) and (year mod 400 = 0)) then IsLeapYear:= True;
+  end;
+ 
+  
+  begin
+   
+  end.  
