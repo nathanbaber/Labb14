a21ef304005631d85ef1e1cad62c2d7414473380
Glukhikh Anastasia <glukhikh@sfedu.ru>
2021-10-21 09:06:36 +0300

    tp

--- DateTime.pas
+++ DateTime.pas
@@ -3,11 +3,7 @@
   begin
     if ((year mod 4 = 0) and (year mod 100 <> 0) or (year mod 100 = 0) and (year mod 400 = 0)) then IsLeapYear:= True;
   end;
-  
-   function SecondsInMinute(min: integer) : integer;
-  begin
-    result:= min * 60;
-  end;
+ 
   
   begin
    
